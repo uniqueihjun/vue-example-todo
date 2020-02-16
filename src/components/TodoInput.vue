c474dd4669d0a0caf225c59d14ce1d1bbbf53d63
ihjun <uniqueihjun@naver.com>
2020-02-16 16:15:06 +0900

    ux and transitions completed

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,28 +4,55 @@
       <span class="addContainer" v-on:click="addTodo">
         <i class="fas fa-plus addBtn" aria-hidden="true"></i>
       </span>
+      <Modal v-if="showModal" @close="showModal = false">
+        <!--
+          you can use custom content here to overwrite
+          default content
+        -->
+        <h3 slot="header">{{ header }}
+          <i class="closeModalBtn fa fa-times" 
+            aria-hidden="true" 
+            @click="showModal = false">
+          </i>
+        </h3>
+        <h3 slot="body">{{ body }}</h3>
+      </Modal>
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
+import Modal from './common/Modal.vue';
 
 export default {
     data : function() {
         return {
             message : '',
-            newTodoItem : ''
+            newTodoItem : '',
+            showModal : false,
+            header : '',
+            body : '',
+            footer : 'copyright'
         }
     },
     methods : {
         addTodo : function() {
-                        
-            this.$emit('addItem', this.newTodoItem);
-            this.clearInput();
+            if (this.newTodoItem !== '') {
+              this.$emit('addItem', this.newTodoItem);
+              this.clearInput();
+            } else {
+              this.header = '정보확인';
+              this.body = '할일을 입력하세요.';
+              this.showModal = !this.showModal;
+            }
+            
         },
         clearInput : function() {
             this.newTodoItem = '';
         }
+    },
+    components : {
+      Modal : Modal
     }
 }
 </script>
@@ -54,5 +81,8 @@ input:focus {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
